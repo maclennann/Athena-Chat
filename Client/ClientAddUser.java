25bcf4022ec334864ada5a793cc4e07ae7fdbf82
Gregory LeBlanc <leblancgwit@gmail.com>
2010-04-15 22:55:16 -0400

    Adduser creates the buddylist file.

--- Client/ClientAddUser.java
+++ Client/ClientAddUser.java
@@ -32,8 +32,8 @@ import javax.swing.border.TitledBorder;
  *
  */
 public class ClientAddUser extends JPanel {
-	
-	
+
+
 	/**
 	 * 
 	 */
@@ -63,7 +63,7 @@ public class ClientAddUser extends JPanel {
 	public JButton cancelJButton = new JButton("Cancel");
 	public ImageIcon redX = new ImageIcon("../images/redX.png");
 	public ImageIcon greenCheck = new ImageIcon("../images/greenCheck.png");
-	
+
 	public Border blackline;
 	public TitledBorder generalTitledBorder;
 	public RSAPublicKeySpec pub;
@@ -78,32 +78,32 @@ public class ClientAddUser extends JPanel {
 		addUserJFrame= new JFrame("User Registration");
 		addUserJFrame.setSize(310,420);
 		addUserJFrame.setResizable(false);
-		
+
 		//Create the content Pane
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		contentPane.setVisible(true);
-				
+
 		//Initalize borders
 		blackline = BorderFactory.createLineBorder(Color.black);
 		generalTitledBorder = BorderFactory.createTitledBorder(
-			       blackline, "Registration Information");
-		
+				blackline, "Registration Information");
+
 		//First Name Input
 		firstNameJTextField = new JTextField();
 		firstNameJLabel.setBounds(15,25,100,25);
 		firstNameJTextField.setBounds(130,25,120,25);
-		
+
 		//Last Name Input
 		lastNameJTextField = new JTextField();
 		lastNameJLabel.setBounds(15,65,100,25);
 		lastNameJTextField.setBounds(130,65,120,25);
-		
+
 		//Email Addres Input
 		emailAddressJTextField = new JTextField();
 		emailAddressJLabel.setBounds(15,105,100,25);
 		emailAddressJTextField.setBounds(130,105,120,25);
-				
+
 		//Confirm Email Address Input
 		confirmEmailAddresJTextField = new JTextField();
 		confirmEmailAddressJLabel.setBounds(15,145,135,25);
@@ -117,13 +117,13 @@ public class ClientAddUser extends JPanel {
 		userNameJTextField = new JTextField();
 		userNameJLabel.setBounds(15,185,100,25);
 		userNameJTextField.setBounds(130,185,120,25);
-		
+
 		//Password Input
 		//TODO Create some way to have an image pop up if they match, etc. Maybe a password strenght meter?
 		passwordJPasswordField = new JPasswordField();
 		passwordJLabel.setBounds(15,225,100,25);
 		passwordJPasswordField.setBounds(130,225,120,25);
-		
+
 		//Confirm Password Input
 		confirmpasswordJPasswordField = new JPasswordField();
 		confirmPasswordJLabel.setBounds(15,265,135,25);
@@ -132,12 +132,12 @@ public class ClientAddUser extends JPanel {
 		passwordMatchesJLabel.setIcon(redX);
 		passwordMessageJLabel.setBounds(15,310,400,25);
 		passwordMessageJLabel.setForeground(Color.RED);
-		
+
 		//Confirm and Cancel JButtons
 		confirmJButton.setBounds(35,345,100,25);
 		cancelJButton.setBounds(160,345,100,25);
 		confirmJButton.setEnabled(false);
-		
+
 		//ActionListener to make the connect menu item connect
 		confirmJButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event){
@@ -145,14 +145,14 @@ public class ClientAddUser extends JPanel {
 				try {
 					//Hash the password
 					password = ClientLogin.computeHash(new String(passwordJPasswordField.getPassword()));
-					
+
 					//Generate the public and private keypair
 					RSACrypto.generateRSAKeyPair();
 					pub = RSACrypto.getPublicKey();
 					priv = RSACrypto.getPrivateKey();
-					
+
 					String newUsername = userNameJTextField.getText();
-					
+
 					//Pull out the key components
 					publicMod = pub.getModulus();
 					publicExp = pub.getPublicExponent();
@@ -162,19 +162,27 @@ public class ClientAddUser extends JPanel {
 					File privKeyFile = new File("users/"+newUsername+"/keys/"+newUsername+".priv");
 					if(!(pubKeyFile.exists())){
 						boolean success = new File("users/"+newUsername+"/keys").mkdirs();
-							System.out.println("Created Directory");
-							pubKeyFile.createNewFile();
-							System.out.println("Created File");
-							}
+						System.out.println("Created Directory");
+						pubKeyFile.createNewFile();
+						System.out.println("Created File");
+					}
 					if(!(privKeyFile.exists())){
 						boolean success = new File("users/"+newUsername+"/keys").mkdirs();
-							privKeyFile.createNewFile();
+						privKeyFile.createNewFile();
 					}
-					
+
+					File buddyList = new File("users/"+newUsername+"/buddylist.csv");
+					if(!(buddyList.exists())){
+						boolean success = new File("users/"+newUsername+"/buddylist.csv").mkdirs();
+						System.out.println("Created Directory");
+						buddyList.createNewFile();
+						System.out.println("Created File");
+					}
+
 					//Write the keys to the file
 					RSACrypto.saveToFile("users/"+newUsername+"/keys/"+userNameJTextField.getText()+".pub",publicMod,publicExp);
 					RSACrypto.saveToFile("users/"+newUsername+"/keys/"+userNameJTextField.getText()+".priv",privateMod,privateExp);
-					
+
 					//System.out.println(firstNameJTextField.getText() + lastNameJTextField.getText() + emailAddressJTextField.getText() + userNameJTextField.getText() + password);
 					//Send the information to Aegis
 					sendInfoToAegis(firstNameJTextField.getText(), lastNameJTextField.getText(), emailAddressJTextField.getText(), userNameJTextField.getText(), password);
@@ -183,9 +191,9 @@ public class ClientAddUser extends JPanel {
 					e.printStackTrace();
 				}
 
-				}
+			}
 		});
-		
+
 		cancelJButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -194,13 +202,13 @@ public class ClientAddUser extends JPanel {
 				addUserJFrame.dispose();
 			} 
 		});
-		
+
 		confirmEmailAddresJTextField.addKeyListener(new KeyListener() {			
 			@Override
 			public void keyTyped(KeyEvent e) {
-				
+
 			}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 				// TODO Auto-generated method stub
@@ -217,7 +225,7 @@ public class ClientAddUser extends JPanel {
 					confirmJButton.setEnabled(false);
 				}				
 			}
-			
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				// TODO Auto-generated method stub
@@ -235,13 +243,13 @@ public class ClientAddUser extends JPanel {
 				}
 			}
 		});
-		
+
 		emailAddressJTextField.addKeyListener(new KeyListener() {			
 			@Override
 			public void keyTyped(KeyEvent e) {
-				
+
 			}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 				if(confirmEmailAddresJTextField.getText().equals(emailAddressJTextField.getText()) && confirmEmailAddresJTextField.getText().indexOf("@") > -1) { 
@@ -256,7 +264,7 @@ public class ClientAddUser extends JPanel {
 					confirmJButton.setEnabled(false);
 				}				
 			}
-			
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if(confirmEmailAddresJTextField.getText().equals(emailAddressJTextField.getText()) && confirmEmailAddresJTextField.getText().indexOf("@") > -1) { 
@@ -272,13 +280,13 @@ public class ClientAddUser extends JPanel {
 				}				
 			}
 		});
-		
+
 		passwordJPasswordField.addKeyListener(new KeyListener() {			
 			@Override
 			public void keyTyped(KeyEvent e) {
-		
+
 			}
-		
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 				String passwordOne = new String(passwordJPasswordField.getPassword());
@@ -296,10 +304,10 @@ public class ClientAddUser extends JPanel {
 					passwordMessageJLabel.setForeground(Color.RED);
 					confirmJButton.setEnabled(false);
 				}	
-			
+
 			}
-			
-	
+
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				String passwordOne = new String(passwordJPasswordField.getPassword());
@@ -318,13 +326,13 @@ public class ClientAddUser extends JPanel {
 				}
 			}
 		});
-		
+
 		confirmpasswordJPasswordField.addKeyListener(new KeyListener() {			
 			@Override
 			public void keyTyped(KeyEvent e) {
-				
+
 			}
-		
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 				String passwordOne = new String(passwordJPasswordField.getPassword());
@@ -343,7 +351,7 @@ public class ClientAddUser extends JPanel {
 					confirmJButton.setEnabled(false);
 				}			
 			}
-				
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				String passwordOne = new String(passwordJPasswordField.getPassword());
@@ -384,27 +392,27 @@ public class ClientAddUser extends JPanel {
 		contentPane.add(cancelJButton);
 		contentPane.add(emailMessageJLabel);
 		contentPane.add(passwordMessageJLabel);
-		
+
 		//Make sure we can see damn thing
 		contentPane.setVisible(true);
 		contentPane.setBorder(generalTitledBorder);
-		
+
 		//Let the Frame know what's up
 		addUserJFrame.setContentPane(contentPane);
 		addUserJFrame.setVisible(true);
 	}
-	
+
 	//This Method will send all of the information over to Aegis for input into the database
 	public void sendInfoToAegis(String firstName, String lastName, String emailAddress, String userName, String password) { 
 
 		//Get a connection
 		Client.connect();
-		
+
 		//Give me back my filet of DataOutputStream + DataInputStream
 		DataOutputStream dout = Client.returnDOUT();
 		DataInputStream din = Client.returnDIN();
-		
-		
+
+
 		try {
 			//Tell the server we're not going to log in
 			//Maybe we should try encrypting this first!
@@ -416,9 +424,9 @@ public class ClientAddUser extends JPanel {
 		}
 		//Invoke Client's systemMessage to tell it what we're about to do, if you know what I mean.	
 		Client.systemMessage("000");
-		
 
-		
+
+
 		//Send Aegis the goods
 		try {
 			//Encrypt information to send to Aegis. Turn them into BigIntegers so we can move them
@@ -428,24 +436,24 @@ public class ClientAddUser extends JPanel {
 			BigInteger emailAddressCipher = new BigInteger(RSACrypto.rsaEncryptPublic(emailAddress,Client.serverPublic.getModulus(),Client.serverPublic.getPublicExponent()));
 			BigInteger userNameCipher = new BigInteger(RSACrypto.rsaEncryptPublic(userName,Client.serverPublic.getModulus(),Client.serverPublic.getPublicExponent()));
 			BigInteger passwordCipher = new BigInteger(RSACrypto.rsaEncryptPublic(password,Client.serverPublic.getModulus(),Client.serverPublic.getPublicExponent()));
-			
+
 			//Send the server the pieces of our public key to be assembled at the other end
 			//TODO These should be encrypted along with everything else
 			//For this test the server needs these numbers to decrypt things
 			dout.writeUTF(publicMod.toString());
 			dout.writeUTF(publicExp.toString());
-			
+
 			//Turn the encrypted data into numbers for
 			//BigInteger firstNameNumber = new BigInteger(firstNameCipher);
-			
+
 			//Send encrypted data to Aegis
 			dout.writeUTF(firstNameCipher.toString());
 			dout.writeUTF(lastNameCipher.toString());
 			dout.writeUTF(emailAddressCipher.toString());
 			dout.writeUTF(userNameCipher.toString());
 			dout.writeUTF(passwordCipher.toString());
-			
-			
+
+
 			//Grab the result
 			String result = din.readUTF();
 			byte[] resultBytes = (new BigInteger(result)).toByteArray();
@@ -467,7 +475,7 @@ public class ClientAddUser extends JPanel {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
