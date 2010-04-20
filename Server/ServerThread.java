473e0c8fd91e2d20769157fd388f7f3b66aa76ac
Norm MacLennan <maclennann@wit.edu>
2010-04-20 17:36:26 -0400

    PUblic

--- Server/ServerThread.java
+++ Server/ServerThread.java
@@ -782,7 +782,9 @@ public class ServerThread extends Thread
 		System.out.println(username);
 		//Acknowledge connection. Make sure we are doing the right thing
 		BigInteger accessGrantedCipher = new BigInteger(RSACrypto.rsaEncryptPrivate("Access granted. Send me the username to find the key for.",serverPrivate.getModulus(),serverPrivate.getPrivateExponent()));
-		sendSystemMessage(username, "ReturnPublicKey");
+		sendSystemMessage(username, "Aegis");
+		serverDout.writeUTF(encryptServerPrivate("ReturnPublicKey"));
+		
 		//serverDout.writeUTF(accessGrantedCipher.toString());
 		try{
 			//Listen for the username
