aff4c1f16913db74223cbdbc2e26e6a2470e7b26
Norm MacLennan <maclennann@wit.edu>
2010-04-21 13:55:06 -0400

    Markdown parser

---
+++ Client/MarkdownTest.java
@@ -0,0 +1,49 @@
+import java.lang.String;
+
+public class MarkdownTest{
+	public static void main (String[] args){
+		String message = args[0];
+		int bold=0;
+		int italic=0;
+		int x=0;
+		char current;
+		char previous=' ';
+		char next=' ';
+		for(x=0;x<message.length();x++){
+			current = message.charAt(x);
+			if(x>0)	previous = message.charAt(x-1);
+			if(x!=message.length()-1) next = message.charAt(x+1);
+			
+			if(current=='*'){
+				if(x>0){
+				if(previous=='\\'){
+					//do nothing
+				}}
+				if(next=='*'){
+					if(bold==1){
+						bold=0;
+						System.out.print("</b>");
+					}
+					else{
+						bold=1;
+						System.out.print("<b>");
+					}
+					x++;
+				}
+				else{
+					if(italic==1){
+						italic=0;
+						System.out.print("</i>");
+					}
+					else{
+						italic=1;
+						System.out.print("<i>");
+					}
+				}
+			}
+			else{
+				System.out.print(current);
+			}
+		}
+	}
+}
