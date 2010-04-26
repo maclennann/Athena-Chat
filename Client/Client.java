c76f19242d03d1c1f2ca4231a556412d8a3437fe
unknown <sk@.(none)>
2010-04-26 11:05:00 -0400

    Added buddy sign off message

--- Client/Client.java
+++ Client/Client.java
@@ -463,6 +463,9 @@ public class Client
 							AudioPlayer.player.start(as);  
 						}
 					}
+					if((usernames[x].equals(decryptedMessage)) && (clientResource.tabPanels.containsKey(decryptedMessage))) { 
+						print.writeToTextArea(decryptedMessage + "has signed off.", print.getSetHeaderFont(Color.red));						
+					}
 				}	
 				return;
 			}
@@ -1375,6 +1378,7 @@ public class Client
 			c2ssocket.close();
 			c2csocket.close();
 			connected=0;
+			away=0;
 			if(clientResource != null){
 				clientResource.setVisible(false);
 			}
