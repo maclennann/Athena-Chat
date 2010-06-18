a9a02d232641429dc7558b62c330a36cb8c5e2f4
Norm MacLennan <maclennann@wit.edu>
2010-06-17 23:39:07 -0400

    userlist transfer

--- Client/Athena.java
+++ Client/Athena.java
@@ -501,7 +501,7 @@ public class Athena
 					systemMessage("14");
 					c2sdout.writeUTF(encryptServerPublic(chatName[2]));
 					clientResource.makeChatTab(chatName[0], chatName[2], true);
-					getUserList(chatName[2]);
+					getUserList(Integer.parseInt(chatName[2]));
 					SecretKeySpec nothing = new SecretKeySpec("lol".getBytes(),"AES");
 					sessionKeys.put(chatName[2],nothing);
 				}
@@ -679,7 +679,7 @@ public class Athena
 		try{
 			systemMessage("18");
 			c2sdout.writeUTF(encryptServerPublic(String.valueOf(chatUID)));
-			String userList = c2sdin.readUTF();
+			String userList = decryptServerPublic(c2sdin.readUTF());
 			String[] users = userList.split(",");
 			for(int x=0;x<users.length;x++){
 				clientResource.newChatListItems(users[x]);
