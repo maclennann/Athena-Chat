71c9acbb279fbd3b1f648b3af3a4fedeb04bfa0a
Norm MacLennan <maclennann@wit.edu>
2010-07-16 23:25:16 -0400

    progressbar fixes

--- Client/LoginProgress.java
+++ Client/LoginProgress.java
@@ -7,7 +7,6 @@ public class LoginProgress extends JFrame {
 	JFrame progressWindow;
 	ImageIcon logoicon = new ImageIcon("images/splash.png");
 	JLabel logo = new JLabel();
-    //JTextArea out;
     JButton find;
 	JLabel currentAction;
     Thread runner;
@@ -20,12 +19,11 @@ public class LoginProgress extends JFrame {
 		int width = (int) scrnsize.getWidth();
 		int height = (int) scrnsize.getHeight();
 		logo.setIcon(logoicon);
-        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel pane = new JPanel();
-		
-		//JOptionPane.showMessageDialog(null,"Window is at: "+(width/2)+"x"+(height/2));
+
         pane.setLayout(new BorderLayout());
 		currentAction = new JLabel(": ");
+		currentAction.setHorizontalAlignment(JLabel.CENTER);
         current = new JProgressBar(0, 2000);
         current.setValue(0);
         current.setStringPainted(true);
@@ -33,7 +31,7 @@ public class LoginProgress extends JFrame {
 		pane.add(current,BorderLayout.CENTER);
         pane.add(currentAction,BorderLayout.PAGE_END);
         setContentPane(pane);
-		super.setLocation(width - (width / 2) - 200, height - (height / 2)-300);
+		super.setLocation(width - (width / 2) - 185, height - (height / 2)-200);
     }
 
 
@@ -44,10 +42,4 @@ public class LoginProgress extends JFrame {
 		super.toFront();
     }
 
-  /*  public static void main(String[] arguments) {
-        Progress frame = new Progress();
-        frame.pack();
-        frame.setVisible(true);
-        frame.iterate();
-   }*/
 }
