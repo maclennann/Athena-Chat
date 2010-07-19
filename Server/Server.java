06826443b103a5f8ae4cecce8004752712220b97
Norm MacLennan <maclennann@wit.edu>
2010-07-19 01:45:11 -0400

    still don't run this

--- Server/Server.java
+++ Server/Server.java
@@ -162,7 +162,7 @@ public class Server {
 
 		//Open the log file for writing
 		openLog(new File("logs/Aegis.txt"));
-
+		while(true){}
 		//Start listening for connections
 		/*ListenerThread listener = new ListenerThread(c2ss,c2css);
 		listener.start();
