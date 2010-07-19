62ab2ca43166c55a029ed3b2b7f464a26ed73da0
Norm MacLennan <maclennann@wit.edu>
2010-07-19 01:48:50 -0400

    lawl

--- Server/Server.java
+++ Server/Server.java
@@ -162,7 +162,7 @@ public class Server {
 
 		//Open the log file for writing
 		openLog(new File("logs/Aegis.txt"));
-		while(true){Thread.sleep(1000000);}
+		while(true){try{Thread.sleep(1000000);}catch(Exception e){}}
 		//Start listening for connections
 		/*ListenerThread listener = new ListenerThread(c2ss,c2css);
 		listener.start();
